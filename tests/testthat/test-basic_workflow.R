e754e0b6e449c4c8edf6f6fa20688ecdbe343a1f
Alice <alice.miller@qut.edu.au>
2022-07-08 11:32:05 +1000

    Finalised test that connection to SQLite database can be established

--- tests/testthat/test-basic_workflow.R
+++ tests/testthat/test-basic_workflow.R
@@ -1,4 +1,4 @@
-test_that("tables can be read from connection to .db file", {
+test_that("connection to SQLite database can be established", {
   sqlitecon <- tweetexploR::connect_to_sqlite_db(test_path("fixtures", "auspol-test.db"))
   expect_s4_class(sqlitecon, "SQLiteConnection")
 })
