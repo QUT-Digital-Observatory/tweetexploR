ce7fbb013d54568a78af0f60ecad1229e54816f4
Alice <alice.miller@qut.edu.au>
2022-07-08 13:55:39 +1000

    Added documentation for num_tweets_by_username()

--- R/basic_workflow.R
+++ R/basic_workflow.R
@@ -44,8 +44,10 @@ configure_y_axis <- function() {
 #'
 #' @examples
 #' \dontrun{
+#'
 #' sqlite_con <- connect_to_sqlite_db("my_database.db")
-#' sqlite_con <- connect_to_sqlite_db("my_subfolder\\my_database.db")}
+#' sqlite_con <- connect_to_sqlite_db("my_subfolder\\my_database.db")
+#' }
 #'
 #' @export
 connect_to_sqlite_db <- function(sqlite_file) {
@@ -53,6 +55,28 @@ connect_to_sqlite_db <- function(sqlite_file) {
 }
 
 
+#' Plot number of tweets for top n usernames
+#'
+#' @description Create a ggplot2 bar chart of the number of tweets per username
+#'   for the top `n` usernames (ties for `n`th position will be included).
+#'
+#' @param sqlite_con [Class SQLiteConnection](https://rsqlite.r-dbi.org/reference/sqliteconnection-class)
+#'   object that is a connection to an SQLite .db file created by the
+#'   [tidy-tweet package](https://github.com/QUT-Digital-Observatory/tidy_tweet).
+#'   The database contains a collection of tweets in relational tables. This can
+#'   be created with [tweetexploR::connect_to_sqlite_db()].
+#'
+#' @param n Number of usernames to be plotted. Note, ties will be included.
+#'
+#' @return ggplot2 plot.
+#'
+#' @examples
+#' \dontrun{
+#'
+#' num_tweets_by_username(sqlite_con, n = 10)
+#' my_plot <- num_tweets_by_username(sqlite_con, 20)
+#' }
+#'
 #' @export
 # Number of tweets by username (top n usernames)
 num_tweets_by_username <- function(sqlite_con, n) {
