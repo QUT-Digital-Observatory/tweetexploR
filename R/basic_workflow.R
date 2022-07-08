b09ef121ee902495409f15084cea568de4c86ee8
Alice <alice.miller@qut.edu.au>
2022-07-08 12:23:29 +1000

    Added documentation for connect_to_sqlite_db()

--- R/basic_workflow.R
+++ R/basic_workflow.R
@@ -24,11 +24,26 @@ configure_y_axis <- function() {
   my_y_axis <- ggplot2::scale_y_continuous(labels = scales::number_format(accuracy = 1, big.mark = ","))
 }
 
+
+#' Connect to tidy-tweet SQLite database
+#'
+#' @description Connect to SQLite database file created by the
+#' [tidy-tweet package](https://github.com/QUT-Digital-Observatory/tidy_tweet).
+#' The database contains a collection of tweets in relational tables.
+#' It is recommended to create a variable for the `SQLiteConnection` created
+#' by `connect_to_sqlite_db()` so that you can use it for other tweetexploR
+#' functions.
+#' @param sqlite_file Path to SQLite .db file created by the
+#' [tidy-tweet package](https://github.com/QUT-Digital-Observatory/tidy_tweet).
+#' The path should end with `.db`
+#' @examples
+#' sqlite_con <- connect_to_sqlite_db("my_database.db")
+#' sqlite_con <- connect_to_sqlite_db("my_subfolder\\my_database.db")
 #' @export
-# Connect to existing .db file in current working directory
 connect_to_sqlite_db <- function(sqlite_file) {
   sqlite_con <- DBI::dbConnect(RSQLite::SQLite(), sqlite_file)
 }
+
 
 #' @export
 # Number of tweets by username (top n usernames)
