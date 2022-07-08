ac4cee40330fe12a8cbd485b3a6f77c5b4c54c54
Alice <alice.miller@qut.edu.au>
2022-07-08 11:31:10 +1000

    Got the first test to work

--- tests/testthat/test-basic_workflow.R
+++ tests/testthat/test-basic_workflow.R
@@ -1,6 +1,4 @@
 test_that("tables can be read from connection to .db file", {
   sqlitecon <- tweetexploR::connect_to_sqlite_db(test_path("fixtures", "auspol-test.db"))
-  tables <- DBI::dbListTables(sqlite_con)
-  expect_length(tables, 7)
+  expect_s4_class(sqlitecon, "SQLiteConnection")
 })
-# This test doesn't pass yet, still trying to get it to work
