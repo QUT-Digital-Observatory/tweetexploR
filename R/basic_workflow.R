66ea7ac79e469d965e04b124dce652f495a1bfa5
Alice <alice.miller@qut.edu.au>
2022-07-08 12:42:30 +1000

    Stopped examples in docs from being run

--- R/basic_workflow.R
+++ R/basic_workflow.R
@@ -29,16 +29,22 @@ configure_y_axis <- function() {
 #'
 #' @description Connect to SQLite database file created by the
 #' [tidy-tweet package](https://github.com/QUT-Digital-Observatory/tidy_tweet).
-#' The database contains a collection of tweets in relational tables.
-#' It is recommended to create a variable for the `SQLiteConnection` created
-#' by `connect_to_sqlite_db()` so that you can use it for other tweetexploR
-#' functions.
+#'   The database contains a collection of tweets in relational tables.
+#'
+#'   It is recommended to create a variable for the `SQLiteConnection` created
+#'   by `connect_to_sqlite_db()` so that you can use it for other tweetexploR
+#'   functions.
+#'
 #' @param sqlite_file Path to SQLite .db file created by the
 #' [tidy-tweet package](https://github.com/QUT-Digital-Observatory/tidy_tweet).
-#' The path should end with `.db`
+#'   The path should end with `.db`
+#'
 #' @examples
+#' \dontrun{
 #' sqlite_con <- connect_to_sqlite_db("my_database.db")
 #' sqlite_con <- connect_to_sqlite_db("my_subfolder\\my_database.db")
+#' }
+#'
 #' @export
 connect_to_sqlite_db <- function(sqlite_file) {
   sqlite_con <- DBI::dbConnect(RSQLite::SQLite(), sqlite_file)
