512cba20ffc2575b0ee8c85d73967f7db3e3f9a7
Alice <alice.miller@qut.edu.au>
2022-07-12 16:40:41 +1000

    Added test cases for daily charts for `check_for_valid_subset_input()`

--- tests/testthat/test-check_for_valid_subset_input.R
+++ tests/testthat/test-check_for_valid_subset_input.R
@@ -1,11 +1,13 @@
-test_that("error is thrown when input for hourly plot is not valid", {
+test_that("warning is thrown when input for hourly plot is not valid", {
   expect_silent(check_for_valid_subset_input(period = "hour", input = "2022-06-20 06:00:00"))
   expect_warning(check_for_valid_subset_input(period = "hour", input = "2022-06-33 06:00:00"))
   expect_warning(check_for_valid_subset_input(period = "hour", input = "2022-06-30"))
 })
 
-test_that("error is thrown when input for daily plot is not valid", {
-  # TODO
+test_that("warning is thrown when input for daily plot is not valid", {
+  expect_silent(check_for_valid_subset_input(period = "day", input = "2022-06-20"))
+  expect_warning(check_for_valid_subset_input(period = "day", input = "2022-06-30 06:00:00"))
+  expect_warning(check_for_valid_subset_input(period = "day", input = "2022-06-33"))
 })
 
 test_that("error is thrown when input for monthly plot is not valid", {
