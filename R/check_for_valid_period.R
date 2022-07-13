bf6569476b0f82fc4c5ce2112e252360d71cca78
Alice <alice.miller@qut.edu.au>
2022-07-13 16:40:23 +1000

    Delete separated functions (will try again in future)

--- R/check_for_valid_period.R
+++
@@ -1,9 +0,0 @@
-# Check for valid `period`
-
-check_for_valid_period <- function(period) {
-  stopifnot(
-    "Please provide a valid value for `period`.
-      Accepted values are \"day\", \"hour\", or \"month\"."
-    = period %in% c("hour", "day", "month")
-  )
-}
