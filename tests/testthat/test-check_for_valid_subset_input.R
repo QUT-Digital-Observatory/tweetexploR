0d9e316be1be3cd628f97b1759742e677d781a7a
Alice <alice.miller@qut.edu.au>
2022-07-13 13:44:42 +1000

    Added more test cases for `check_for_valid_subset_input()`

--- tests/testthat/test-check_for_valid_subset_input.R
+++ tests/testthat/test-check_for_valid_subset_input.R
@@ -2,16 +2,19 @@ test_that("warning is thrown when input for hourly plot is not valid", {
   expect_silent(check_for_valid_subset_input(period = "hour", input = "2022-06-20 06:00:00"))
   expect_warning(check_for_valid_subset_input(period = "hour", input = "2022-06-33 06:00:00"))
   expect_warning(check_for_valid_subset_input(period = "hour", input = "2022-06-30"))
+  expect_warning(check_for_valid_subset_input(period = "hour", input = "2022-06"))
 })
 
 test_that("warning is thrown when input for daily plot is not valid", {
   expect_silent(check_for_valid_subset_input(period = "day", input = "2022-06-20"))
   expect_warning(check_for_valid_subset_input(period = "day", input = "2022-06-30 06:00:00"))
   expect_warning(check_for_valid_subset_input(period = "day", input = "2022-06-33"))
+  expect_warning(check_for_valid_subset_input(period = "day", input = "2022-06"))
 })
 
 test_that("warning is thrown when input for monthly plot is not valid", {
   expect_silent(check_for_valid_subset_input(period = "month", input = "2022-06"))
+  expect_warning(check_for_valid_subset_input(period = "month", input = "2022-13"))
   expect_warning(check_for_valid_subset_input(period = "month", input = "2022-06-01"))
   expect_warning(check_for_valid_subset_input(period = "month", input = "2022-06-01 01:00:00"))
 })
