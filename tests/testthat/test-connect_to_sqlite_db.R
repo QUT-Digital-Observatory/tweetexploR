a9d4e32103236b146710c7b5fb2f82803a6d265f
Alice <alice.miller@qut.edu.au>
2022-07-13 15:33:09 +1000

    Move connection to database to outside the test

--- tests/testthat/test-connect_to_sqlite_db.R
+++ tests/testthat/test-connect_to_sqlite_db.R
@@ -1,7 +1,11 @@
+# Connect to sqlite .db file
+sqlite_con <- connect_to_sqlite_db(test_path("fixtures", "auspol-test.db"))
+
+
 test_that("connection to SQLite database can be established", {
-  sqlite_con <- connect_to_sqlite_db(test_path("fixtures", "auspol-test.db"))
   expect_s4_class(sqlite_con, "SQLiteConnection")
 })
+
 
 # Disconnect from database
 DBI::dbDisconnect(sqlite_con)
