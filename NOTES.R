84c3fde7475147c02dd855a4d71faf89231b3c30
Alice <alice.miller@qut.edu.au>
2022-07-14 14:50:18 +1000

    Added a NOTES.R file for recording notes/questions/etc.

---
+++ NOTES.R
@@ -0,0 +1,5 @@
+# Using DESCRIPTION.Imports vs. @importFrom package function inside each function
+
+# Currently these functions are using @importFrom:
+#  num_tweets_by_timeperiod()
+#  num_users_by_timeperiod()
