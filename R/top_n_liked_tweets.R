f9296f3afc70f99c4b4af34e3643142ba3fa76c5
Alice <alice.miller@qut.edu.au>
2022-08-24 12:02:38 +1000

    Progress towards #4 for `top_n_liked_tweets()`

--- R/top_n_liked_tweets.R
+++ R/top_n_liked_tweets.R
@@ -17,9 +17,16 @@
 #' @param chars_per_line How many characters of the tweet text to be displayed
 #'   per line without breaking a word.
 #'
+#' @param return_data Should the data underlying the chart be returned?
+#'   The default is `FALSE`. If `return_data = TRUE`, the data can be accessed
+#'   in the second element, `data`, of the returned list.
+#'
 #' @param ... Other arguments passed on to [ggplot2::geom_col()].
 #'
-#' @return ggplot2 plot.
+#' @return ggplot2 plot. If `return_data = TRUE`, returns a named list with the
+#'   first element, `chart`, being a ggplot2 plot, and the second element,
+#'   `data`, being the underlying data for the ggplot2 plot in the form of a
+#'   data frame.
 #'
 #' @importFrom dplyr slice_max
 #'
@@ -39,18 +46,23 @@
 #'
 #' top_n_liked_tweets(sqlite_con, fill = "blue")
 #'
+#' top_50_liked_tweets <- top_n_hashtags(sqlite_con, n = 50, return_data = TRUE)
+#'
 #' }
 #'
 #' @export
 
 top_n_liked_tweets <- function(sqlite_con, n = 10, tweet_chars = 80,
-                               chars_per_line = 50, ...) {
-  DBI::dbGetQuery(sqlite_con,
+                               chars_per_line = 50, return_data = FALSE, ...) {
+
+  chart_data <- DBI::dbGetQuery(sqlite_con,
   "SELECT id, like_count, text
   FROM tweet;") %>%
-    slice_max(n = n, order_by = .data$like_count, with_ties = TRUE) %>%
-    ggplot(aes(x = reorder(substr(.data$text, 1, tweet_chars),
-                           .data$like_count), .data$like_count)) +
+    slice_max(n = n, order_by = .data$like_count, with_ties = TRUE)
+
+  chart <- ggplot(chart_data, aes(x = reorder(substr(.data$text, 1, tweet_chars),
+                                              .data$like_count),
+                                  y = .data$like_count)) +
     geom_col(...) +
     labs(title = paste0("Top ", n, " liked tweets (Twitter metrics)"),
          x = "Tweet",
@@ -60,4 +72,13 @@ top_n_liked_tweets <- function(sqlite_con, n = 10, tweet_chars = 80,
     coord_flip() +
     configure_ggplot_theme() +
     theme(axis.title.y = element_blank())
+
+  if (return_data == TRUE) {
+    return(list(chart = chart, data = chart_data))
+  }
+
+  else if (return_data == FALSE) {
+    return(chart)
+  }
+
 }
--- tests/testthat/test-top_n_liked_tweets.R
+++ tests/testthat/test-top_n_liked_tweets.R
@@ -2,6 +2,8 @@
 sqlite_con <- connect_to_sqlite_db(test_path("fixtures", "auspol-test.db"))
 
 
+# Tests for when return_data = FALSE
+
 test_that("result is a ggplot2 object", {
   expect_true(ggplot2::is.ggplot(top_n_liked_tweets(sqlite_con, 10)))
 })
@@ -10,6 +12,37 @@ test_that("result is a ggplot2 object", {
 test_that("ggplot2 plot has expected output", {
   vdiffr::expect_doppelganger("top_n_liked_tweets_10",
                               top_n_liked_tweets(sqlite_con, 10))
+})
+
+
+# Tests for when return_data = TRUE
+
+test_that("list of length 2 is created as expected", {
+  top_50_liked_tweets <- top_n_liked_tweets(sqlite_con, n = 50,
+                                            return_data = TRUE)
+  expect_type(top_50_liked_tweets, "list")
+  expect_equal(2, length(top_50_liked_tweets))
+})
+
+test_that("first element of list (chart) is a list", {
+  top_50_liked_tweets <- top_n_liked_tweets(sqlite_con, n = 50,
+                                            return_data = TRUE)
+  expect_type(top_50_liked_tweets$chart, "list")
+})
+
+
+test_that("second element of list (data) is a data frame", {
+  top_50_liked_tweets <- top_n_liked_tweets(sqlite_con, n = 50,
+                                            return_data = TRUE)
+  expect_true(is.data.frame(top_50_liked_tweets$data))
+})
+
+
+test_that("ggplot2 plot has expected output", {
+  top_20_liked_tweets <- top_n_liked_tweets(sqlite_con, n = 20,
+                                            return_data = TRUE)
+  vdiffr::expect_doppelganger("top_20_liked_tweets",
+                              top_20_liked_tweets$chart)
 })
 
 
